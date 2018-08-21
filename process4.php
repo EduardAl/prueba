973f794291864002d80c5da35c939bdb343acb2c
Eduardo <eduard_alfons@hotmail.com>
2018-08-21 03:37:41 -0600

    payment

---
+++ process4.php
@@ -0,0 +1,18 @@
+
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     <title></title>
+   </head>
+   <body>
+   <div align="center">
+        <fieldset><legend style="text-align: center;">Confirmation </legend>
+        <form action="process4.php" method="POST">
+            <h1 >Payment successful</h1>
+           
+        </form>
+        </fieldset>
+    </div>
+   </body>
+ </html>
