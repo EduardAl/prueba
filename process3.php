7e7267f5d50c2bba69fd039c3d01288d62db371f
Eduardo <eduard_alfons@hotmail.com>
2018-08-21 03:01:50 -0600

    process3

---
+++ process3.php
@@ -0,0 +1,46 @@
+<?php
+
+$fname = $_POST['fname'];
+$lname = $_POST['lname'];
+$bdate = $_POST['bdate'];
+$cnumber = $_POST['cnumber'];
+$valid = $_POST['valid'];
+$holder = $_POST['holder'];
+$cvc = $_POST['cvc'];
+$doc = $_POST['doc'];
+$dnumber = $_POST['dnumber'];
+$email = $_POST['email'];
+$array_doc = [
+    'DUI',
+    'NIT'
+];
+$price = $_POST[price];
+$quantity = $_POST[quantity];
+$discount = $_POST[discount];
+ ?>
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     <title></title>
+   </head>
+   <body>
+
+
+
+       <h1 >Information</h1>
+       <p>First Name: <b><?php echo $fname; ?></b></p>
+       <p>Last Name: <b><?php echo $lname; ?></b></p>
+       <p>Birthdate: <b><?php echo $ddate; ?></b></p>
+       <p><?php echo $array_doc[$doc-1];?>: <b><?php echo $dnumber; ?></b></p>
+       <p>Email: <b><?php echo $email; ?></b></p>
+       <p>Unit price : <b><?php echo $price; ?></b></p>
+       <p>Quantity: <b><?php echo $quantity; ?></b></p>
+       <p>Total without discount: <b><?php echo $price*$quantity; ?></b></p>
+       <p>Total with discount: <b><?php echo ($price*$quantity)*(1-($discount/100)); ?></b></p>
+       <p>
+            
+       </p>
+
+   </body>
+ </html>
