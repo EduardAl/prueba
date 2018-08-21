6174f662f6316ae664c5029ae759918c2db73a38
Eduardo <eduard_alfons@hotmail.com>
2018-08-21 03:54:55 -0600

    a

--- process4.php
+++ process4.php
@@ -4,14 +4,19 @@
    <head>
      <meta charset="utf-8">
      <title></title>
-     
+     <script language="javascript" type="text/javascript">
+     function send(page){
+        document.formname.action = page;
+        document.formname.submit();
+
+}
    </head>
    <body>
    <div align="center">
         <fieldset><legend style="text-align: center;">Confirmation </legend>
         <form action="process4.php" method="POST">
             <h1 >Payment successful</h1>
-            <input type="button" onclick="location.info.html" name="back" value="Go home"><br>
+            <input type="button" onclick="send('info.html')" name="go" value="Go home"><br>
         </form>
         </fieldset>
     </div>
