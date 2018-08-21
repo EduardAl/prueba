4d05a0508f740c102dc3d8983e02d8221827d90a
Eduardo <eduard_alfons@hotmail.com>
2018-08-21 03:43:40 -0600

    a

--- process4.php
+++ process4.php
@@ -4,13 +4,19 @@
    <head>
      <meta charset="utf-8">
      <title></title>
+     <script languaje="javascript">
+        function limpiarform()
+            {
+                document.info.html.reset();
+            }
+</script>
    </head>
    <body>
    <div align="center">
         <fieldset><legend style="text-align: center;">Confirmation </legend>
         <form action="process4.php" method="POST">
             <h1 >Payment successful</h1>
-            <input type="button" onclick="history.back(history.back())" name="back" value="Go home"><br>
+            <input type="button" onclick=" limpiarform(); history.back(history.back())" name="back" value="Go home"><br>
         </form>
         </fieldset>
     </div>
